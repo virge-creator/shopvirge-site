949bb46f19914b2fee8b7494dca7f2249ee403ef
Rene Dohmen <acidjunk@Mac.fritz.box>
2026-05-05 14:27:24 +0200

    fix: add tailwind css import + vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
+  vite: { plugins: [tailwindcss()] },
   site: 'https://virge-creator.github.io',
   base: '/shopvirge-site/',
   i18n: {
