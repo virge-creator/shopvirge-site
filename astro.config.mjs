b87505bf523beaa6bd041e825ddbfa57114851a0
Rene Dohmen <acidjunk@Mac.fritz.box>
2026-05-05 14:15:33 +0200

    add CNAME for shopvirge.com + GitHub Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://shopvirge.com',
-  base: '/',
+  site: 'https://virge-creator.github.io',
+  base: '/shopvirge-site/',
   i18n: {
     defaultLocale: 'nl',
     locales: ['nl', 'en'],
